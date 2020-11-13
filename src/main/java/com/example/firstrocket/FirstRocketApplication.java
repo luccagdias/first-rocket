93e695443cca47029a1028bc61522725c21e8603
Lucca Dias <lucca.gdias07@gmail.com>
2020-11-13 20:19:28 -0300

    Initial Project

---
+++ src/main/java/com/example/firstrocket/FirstRocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstrocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firstrocket/FirstRocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstrocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstRocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
